0a30e93c38892f7b706d14559a3812b5d53444fd
Mat <matshot03@gmail.com>
2021-12-03 07:06:43 +0100

    Ajout défi

---
+++ Un darkmode à la mode/js/darkmode.js
@@ -0,0 +1,7 @@
+function afficher_doc(){
+    document.getElementById('doc').innerHTML="Ce site web permet de visiter différents site comme celui de la nuit de l'info ou même le site de l'entreprise à l'origine de ce défi (Norsys). <br><br> Il permet également de rejoindre le discord officiel de la nuit de l'info, rejoindre le discord de notre lycée mais aussi de consulter nos différents projets. <br><br> En haut de ce site web vous retrouverez l'header servant à naviguer sur différents sites web. <br><br> L'élément le plus important de ce site est le bouton DARKMODE/LIGHTMODE, il permet de passer ce site web d'un thème sombre à un thème clair juste en cliquant dessus grace à différentes lignes de code. <br><br> Pour la mise en page nous utilisons Bootstrap qui permet de gérer nos différents éléments comme nous le souhaitons en rendant par la même occasion, le site responsive (ou partiellement).";
+}
+
+function effacer_doc(){
+    document.getElementById('doc').innerHTML="";
+}
---
+++ Un darkmode à la mode/readme.txt
@@ -0,0 +1,10 @@
+------------------------------{Document Readme}------------------------------
+
+- Comment démarer le site web ?
+
+--> Afin de démarer le site web, il vous suffit simplement de récupérer le dossier complet "Projet_Darkmode".
+Ensuite faite un clic droit sur le fichier index.html > ouvrir avec.. et séléctionnez le navigateur de votre choix ou simplement double-cliquez sur le fichier. 
+
+- Comment fonctionne le site web ?
+
+--> Le fonctionnement du site est relativement simple. Nous vous invitons simplement à suivre les instructions données sur le site.
